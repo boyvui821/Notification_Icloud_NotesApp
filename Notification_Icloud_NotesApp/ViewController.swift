114f298350a8ead665693161ea5bd6720e29b06f
Nguyen Hieu Trung <ktpm11520669@gmail.com>
2018-10-27 00:25:34 +0700

    Initial Commit

---
+++ Notification_Icloud_NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notification_Icloud_NotesApp
+//
+//  Created by Nguyen Hieu Trung on 10/27/18.
+//  Copyright © 2018 OneSignal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
